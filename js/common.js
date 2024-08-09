6abb30b2f30d42293adb9311025a27e3b7e5881d
soyeon kim <soyeon.kim@mbaas.kr>
2024-08-09 17:17:14 +0900

    추가내용

--- js/common.js
+++ js/common.js
@@ -1,8 +1,32 @@
 
 $(document).ready(function () {
+  // input 파일
   $("#file").on('change',function(){
     var fileName = $("#file").val();
     $(".upload-name").val(fileName);
   });
 
+  // 좋아요, 저장 버튼
+  $('.detail button ').click(function () {
+    $(this).toggleClass("on")
+  });
+
+  // 팝업
+  $('.open').each(function() {
+		var popupID = $(this).attr('href');
+		var popupBg = $('.popup-wrap');
+	
+		$(this).on('click', function(e) {
+			e.preventDefault();
+			popupBg.fadeIn();
+			$(popupID).show();
+			$('html').css({overflow: 'hidden'});
+		});
+		$('.popup-wrap, .close').on('click', function() {
+			popupBg.fadeOut();
+			$(popupID).hide();
+			$(popupID).removeAttr('on');
+			$('html').removeAttr('on');
+		});
+	});
 });
